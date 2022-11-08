d4427e5e2553d003c9e57dd66a7cfc8c675a6779
Anxo Vilas <anxovilasm@gmail.com>
2022-11-08 13:48:38 +0100

    Añadido un contador y arreglado el spawn de enemigos para asegurarse que aparecen dentro de los límites

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ class Canvas {
     constructor(canvas) {
         this.canvas = canvas;
         this.ctx = this.canvas.getContext("2d");
+        this.ctx.textAlign = "center";
+        this.ctx.textBaseline = "middle";
+        this.ctx.font = "400 500px sans-serif";
     }
     
     /**
@@ -13,6 +16,9 @@ class Canvas {
     refresh() {
         this.ctx.fillStyle = "#fff";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.fillStyle = "hsla(0, 0%, 25%, 60%)";
+        this.ctx.fillText(player.points, this.canvas.width/2, this.canvas.height/2);
+
     }
 
     /**
@@ -72,13 +78,21 @@ class Canvas {
     }
 }
 
+class Player {
+    constructor() {
+        this.points = 0;
+    }
+}
+
 class Tank {
     /**
      * @param {HTMLCanvasElement} canvas The HTMLElement of the canvas
+     * @param {Player} player The player
      * @param {{x: Number, y: Number}} xy TYhe position of the tank
      */
-    constructor(canvas, xy) {
+    constructor(canvas, player, xy) {
         this.canvas = canvas;
+        this.player = player;
         this.xy = xy;
         this.cxy = {x: this.xy.x+50, y: this.xy.y+38};
         this.r = 0;
@@ -106,6 +120,7 @@ class Tank {
      */
     shoot() {
         proyectiles.push(new Proyectile(this.canvas,
+                                        this,
                                         Object.create({
                                             x: this.cxy.x+100*Math.cos(this.r),
                                             y: this.cxy.y+100*Math.sin(this.r)}),
@@ -116,11 +131,13 @@ class Tank {
 class Proyectile {
     /**
      * @param {HTMLCanvasElement} canvas The HTMLElement of the canvas
+     * @param {Tank} tank The tank of the proyectile
      * @param {{x: Number, y: Number}} xy The position of the proyectile
      * @param {{x: Number, y: Number}} vxy The velocity of the proyectile
      */
-    constructor(canvas, xy, vxy) {
+    constructor(canvas, tank, xy, vxy) {
         this.canvas = canvas;
+        this.tank = tank;
         this.xy = xy;
         this.vxy = vxy;
     }
@@ -150,12 +167,13 @@ class Proyectile {
             if (d < 25) {
                 collided = true;
                 enemies.splice(enemies.indexOf(enemies[i]), 1);
+                tank.player.points++;
             }
         }
 
         if (this.xy.x < 0 || this.xy.x > this.canvas.width || this.xy.y < 0 || this.xy.y > this.canvas.height || collided) {
             proyectiles.splice(proyectiles.indexOf(this), 1);
-    }
+        }
     }
 }
 
@@ -183,9 +201,9 @@ function keydown(e) {
         map.push(key);
 
     if (map.includes("ArrowLeft")) {
-        tank.a -= 3;
+        tank.a -= 2;
     } else if (map.includes("ArrowRight")) {
-        tank.a += 3;
+        tank.a += 2;
     }
     // Recalculate the angle of the tank in radians
     tank.r = tank.a*Math.PI/180;
@@ -221,7 +239,7 @@ function cycle() {
  */
 function spawnEnemy(bound) {
     if (enemies.length < 5) {
-        enemies.push(new Enemy(canvas, {x: Math.random()*(bound.w)+bound.x, y: Math.random()*(bound.h)+bound.y}));
+        enemies.push(new Enemy(canvas, {x: Math.random()*(bound.w-bound.x)+bound.x, y: Math.random()*(bound.h-bound.y)+bound.y}));
     }
 }
 
@@ -232,14 +250,15 @@ let proyectiles = [];
 let enemies = [];
 
 let canvas = new Canvas(document.getElementById("canvas"));
-let tank = new Tank(canvas, {x: 100, y: canvas.canvas.height/2-75});
+let player = new Player();
+let tank = new Tank(canvas, player, {x: 100, y: canvas.canvas.height/2-75});
 // Autospawn an enemy
-spawnEnemy({x: 300, y: 0, w: canvas.canvas.width, h: canvas.canvas.height});
+spawnEnemy({x: 300, y: 0, w: canvas.canvas.width-20, h: canvas.canvas.height-20});
 // Main interval
 setInterval(cycle, 10);
 
 // Enemy spawner interval
-setInterval(() => spawnEnemy({x: 300, y: 0, w: canvas.canvas.width, h: canvas.canvas.height}), 5000);
+setInterval(() => spawnEnemy({x: 300, y: 0, w: canvas.canvas.width-20, h: canvas.canvas.height-20}), 5000);
 
 addEventListener("keydown", keydown);
 addEventListener("keyup", keyup);
